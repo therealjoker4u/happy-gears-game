691a16787ffb85ed69b03b9851639abceff77bcf
Alireza Roosta <alireza.roosta.1998@gmail.com>
2020-08-31 08:00:15 +0430

    Main menu UI is ok but has not reaction

---
+++ Assets/Scripts/SimpleRotation.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleRotation : MonoBehaviour
+{
+    public float speed;
+    void Update()
+    {
+        transform.RotateAround(transform.position, Vector3.back, Time.deltaTime * speed);
+    }
+}
